da7018b87ba31afe1774827b7e261ee678dd06d8
xzy <287368384@qq.com>
2019-03-21 15:47:14 +0800

    创建新的帖子详情页

---
+++ app/src/main/java/com/chuangsheng/forum/ui/forum/ui/NewCommunityDetailActivity.java
@@ -0,0 +1,35 @@
+package com.chuangsheng.forum.ui.forum.ui;
+
+import com.chuangsheng.forum.base.BaseActivity;
+
+/**
+ * Created by tianou on 2019/3/21.
+ */
+
+public class NewCommunityDetailActivity extends BaseActivity {
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void getResLayout() {
+
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void setStatusBarColor() {
+
+    }
+}
